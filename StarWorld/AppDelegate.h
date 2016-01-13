882196b43a262aa3b0ed46d972313e4288d7f00b
孙滢贺 <sunyinghe@sunyinghedeMac.local>
2016-01-12 18:27:53 -0800

    Initial Commit

---
+++ StarWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarWorld
+//
+//  Created by 孙滢贺 on 16/1/12.
+//  Copyright (c) 2016年 YH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
